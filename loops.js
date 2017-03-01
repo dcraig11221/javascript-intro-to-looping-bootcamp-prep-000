61e8cd0f563cc9d224567fd82cda19da67f0b375
Douglas Craig <dcraig112@gmail.com>
2017-03-01 14:00:08 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,32 @@
+var myArray = []
+function forLoop(myArray) {
+	for (i = 0; i < 25; i++) {
+		if (i < 1) {
+			myArray.push(`I am 1 strange loop.`)
+		}
+		else {
+			myArray.push(`I am ${i} strange loops.`)
+		}
+	}	return myArray
+}
+
+function whileLoop(number) {
+	while (number > 0) {
+		console.log(number)
+		number--
+	}
+	return 'done'
+}
+
+//Define maybeTrue()
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+	do {
+		array.pop()
+	}
+	while (array.length > 0 && maybeTrue())
+	return array
+}
