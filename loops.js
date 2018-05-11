39fa79af21902f5d482ec8294f04cf5417b00622
Douglas Craig <dcraig112@gmail.com>
2018-05-11 16:16:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,45 +1,10 @@
-<<<<<<< HEAD
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
-var array = []
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i == 0) {
-      array.push("I am 1 strange loop.")
+  for (let i = 0; i < 25, i++) {
+    if (i = 0){
+    array.push(`I am 1 strange loop.`)
     }
     else {
       array.push(`I am ${i + 1} strange loops.`)
     }
-  }
-  return array
-}
-function whileLoop(n) {
-  while (n > 0) {
-  console.log(n)
-  n--
-  }
-  return "done"
-}
-function doWhileLoop(array) {
-  do {
-    array.pop()
-  }
-  while (array.length > 0 && maybeTrue())
-  return array
-=======
-var array = []
-
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i == 0) {
-      array.push('I am 1 strange loop.')
-    }
-    else {
-      array.push(`I am ${i+1} strange loops.`)
-    }
-  }
-  return array
->>>>>>> f08c8a7019db9395185405492f62a35bf6692306
+    return array
 }
